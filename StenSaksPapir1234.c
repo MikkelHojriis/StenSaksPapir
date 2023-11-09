e5b3fd9e8252818531f542fc9b6b7853ef838026
Mikkel Højriis Jørgensen <149662955+MikkelHojriis@users.noreply.github.com>
2023-11-09 11:47:49 +0100

    Update StenSaksPapir1234.c

--- StenSaksPapir1234.c
+++ StenSaksPapir1234.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> // Inkludér time.h for at generere tilfældige tal ved brug af tid
+#include <time.h> // InkludÃ©r time.h for at generere tilfÃ¦ldige tal ved brug af tid
 
 int main() {
-    // Generer et tilfældigt tal ved brug af tid.
+    // Generer et tilfÃ¦ldigt tal ved brug af tid.
     srand(time(0));
 
     int choice; //Lav Int for ens eget valg
     int computerChoice; //Lav Int for computerens valg
+    int computerPoint = 0;
+    int spillerPoint = 0;
+    int uafgjort = 0;
 
     while (1) { //Lav menu
+        printf("-----------------------\n");
         printf("Vaelg en handling:\n");
         printf("1. Sten\n");
         printf("2. Saks\n");
         printf("3. Papir\n");
-        printf("0. Afslut spillet\n");
+        printf("0. Afslut spillet\n\n");
+        printf("Spiller point: %d\n", spillerPoint);
+        printf("Computer point: %d\n", computerPoint);
+        printf("Uafgjort: %d\n\n", uafgjort);
 
-        // Få brugerens valg
+        // FÃ¥ brugerens valg
         scanf("%d", &choice);
 
         if (choice == 0) {
-            printf("Spillet afsluttet. Farvel!\n");
-            break; // Gå ud af spillet
+            printf("Spillet afsluttet. Farvel!\n\n");
+                if (spillerPoint>computerPoint)
+                {   printf("-----------------------\n");
+                    printf("SPILLEREN VANDT!\n");
+                    printf("-----------------------\n"); }
+                else if (spillerPoint<computerPoint)
+                {   printf("-----------------------\n");
+                    printf("COMPUTEREN VANDT!\n");
+                    printf("-----------------------\n"); }
+                else
+                {   printf("-----------------------\n");
+                    printf("SPILLET ENDTE UAFGJORT!\n");
+                    printf("-----------------------\n"); }
+            break; // GÃ¥ ud af spillet
         } else if (choice < 1 || choice > 3) {
             printf("Ugyldigt valg. Proev igen.\n");
-        } else { // Generer tilfældigt valg indenfor scope af 3 + 1)
+        } else { // Generer tilfÃ¦ldigt valg indenfor scope af 3 + 1)
             computerChoice = rand() % 3 + 1;
 
             // Display the choices made by the user and the computer
@@ -55,11 +74,21 @@ int main() {
 
             // Bestem vinderen
             if (choice == computerChoice) {
-                printf("Uafgjort!\n"); // && Betyder sammenligne
+                printf("Uafgjort!\n\n"); // && Betyder sammenligne
+                uafgjort++;
             } else if ((choice == 1 && computerChoice == 2) || (choice == 2 && computerChoice == 3) || (choice == 3 && computerChoice == 1)) {
-                printf("Du vinder!\n");
+                printf("Du vinder!\n\n");
+                spillerPoint++;
             } else {
-                printf("Computeren vinder!\n");
+                printf("Computeren vinder!\n\n");
+                computerPoint++;
+            }
+        }
+    }
+
+    return 0;
+}
+
             }
         }
     }
